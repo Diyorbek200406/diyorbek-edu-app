f35f2c844a48dc20bada9152d2f3f84ab47cdb52
Erkinov Diyorbek <erkinovdiyorbek39@@gmail.com>
2023-09-17 21:35:02 +0500

    tag component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
-import { Heading, Text } from '../components';
+import { Heading, Tag, Text } from '../components';
 
 const Index = () => {
 	return (
 		<div>
 			<Heading tag='h3'>Hello World!</Heading>
 			<Text size='l'>Text Component</Text>
+			<Tag size='m' color='red'>
+				Red
+			</Tag>
+
+			<Tag size='m' color='green'>
+				Green
+			</Tag>
+
+			<Tag size='m' color='primary'>
+				Primary
+			</Tag>
 		</div>
 	);
 };
