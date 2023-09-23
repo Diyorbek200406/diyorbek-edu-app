a0280dcc5c7af6791013451e1db49b1c07163cb3
Erkinov Diyorbek <erkinovdiyorbek39@@gmail.com>
2023-09-23 12:10:38 +0500

    footer component

--- src/layout/footer/footer.tsx
+++ src/layout/footer/footer.tsx
@@ -1,7 +1,16 @@
 import { FooterProps } from './footer.props';
+import styles from './footer.module.css';
+import cn from 'classnames';
+import { format } from 'date-fns';
 
-const Footer = ({ ...props }: FooterProps): JSX.Element => {
-	return <div {...props}>Footer</div>;
+const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+	return (
+		<footer className={cn(className, styles.footer, {})} {...props}>
+			<div>Erkinov Diyorbek © 2022 - {format(new Date(), 'yyyy')}. All rights reserved</div>
+			<a href='#'>Terms of use</a>
+			<a href='#'>Privacy Policy</a>
+		</footer>
+	);
 };
 
 export default Footer;
