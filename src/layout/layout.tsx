3110a5faae29a861a69aab27c3a8fa654a3f280c
Erkinov Diyorbek <erkinovdiyorbek39@@gmail.com>
2023-09-23 11:51:15 +0500

    shablon

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -3,16 +3,15 @@ import Footer from './footer/footer';
 import Header from './header/header';
 import { LayoutProps } from './layout.props';
 import Sidebar from './sidebar/sidebar';
+import styles from './layout.module.css';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
 	return (
-		<div>
-			<Header />
-			<div>
-				<Sidebar />
-				<div> {children}</div>
-			</div>
-			<Footer />
+		<div className={styles.wrapper}>
+			<Header className={styles.header} />
+			<Sidebar className={styles.sidebar} />
+			<div className={styles.body}>{children}</div>
+			<Footer className={styles.footer} />
 		</div>
 	);
 };
